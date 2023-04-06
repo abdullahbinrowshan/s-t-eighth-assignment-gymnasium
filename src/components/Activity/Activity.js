14dbae5b5ce466aa5436de9f5b5cde05957d1e6d
Abdullah Bin Rowshan <abdullahbinrowshan@gmail.com>
2023-04-06 17:46:40 +0600

    the main activities container is done

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import './Activity.css'
 
 const Activity = ({activity}) => {
-    const {title, description, time, img} = activity
+    const {title, description, age, time, img} = activity
+
+    let cutAbout = ''; 
+
+    if (description.length >= 100) {
+        cutAbout = description.slice(0, 100)
+    }
+
     return (
-        <div>
+        <div className='activity'>
             <img src={img} alt="" />
-            <h1>{title} </h1>
-            <p>{description} </p>
-            <p><span>{time} </span> </p>
+            <h3>{title} </h3>
+            <p className='about' title={description}>{cutAbout ? cutAbout + '...' : description} </p>
+            <p>For Age: <b>17-{age}</b> </p>
+            <p> Time required: <b>{time}s</b> </p>
+            <div className='btn-container'>
+                <button>Add to list</button>
+            </div>
         </div>
     );
 };
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,20 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import Activity from '../Activity/Activity';
+import './Main.css';
+import Cart from '../Cart/Cart';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
+
 
 const Main = () => {
     const [activities, setActivities] = useState([])
 
     useEffect(() => {
         fetch('data.json')
-         .then(res => res.json())
-         .then(data => setActivities(data))
+            .then(res => res.json())
+            .then(data => setActivities(data))
     }, [])
     return (
-        <div>
-            {
-                activities.map(activity => <Activity activity={activity} key={activity.id} /> )
-            }
-            <h1>HELLO WORLD!</h1>
+        <div className='main-container'>
+            <div className="activities-container">
+                <h1 className='heading'> <span><FontAwesomeIcon className='rotate' icon={faDumbbell} /></span> GYMNASIUM-CLUB </h1>
+                <h4>Age Select today’s exercise</h4>
+                <div className='activity-container'>
+                    {
+                        activities.map(activity => <Activity activity={activity} key={activity.id} />)
+                    }
+                </div>
+            </div>
+            <div className="cart-container">
+                <Cart />
+            </div>
         </div>
     );
 };
